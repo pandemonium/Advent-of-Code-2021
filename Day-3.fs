3c99955ed2e29b3a40225f1de23da3bb7dcf066b
Patrik Andersson <patrik.andersson@bynk.se>
2021-12-03 23:42:41 +0100

    Completed the third day.

--- Day-3.fs
+++ Day-3.fs
@@ -5,30 +5,52 @@ open System
 let input =
   IO.File.ReadLines "input-3.txt"
   |> List.ofSeq
-
 //  [ "00100"; "11110"; "10110"; "10111"; "10101"; "01111"; "00111"; "11100"; "10000"; "11001"; "00010"; "01010" ]
 
-//let mostCommonBit 
+let frequencies bits =
+  let ones, zeroes = List.partition ((=) '1') bits
+  in List.length ones, List.length zeroes
 
-let compute =
+let number (xs : char list) =
+  Convert.ToInt32 (List.toArray xs |> String, 2)
+
+let mostCommon =
+  function a, b when a >= b -> '1' | _ -> '0'
+
+let leastCommon =
+  function a, b when a < b -> '1' | _ -> '0'
+
+let compute1 =
   let diagnosticReport =
     input
     |> List.map List.ofSeq
     |> List.transpose
 
-  let frequencies bits =
-    let ones, zeroes = List.partition ((=) '1') bits
-    in List.length ones, List.length zeroes
-
-  let gamma =
-    function a, b when a > b -> '1' | _ -> '0'
-
-  let epsilon =
-    function a, b when a < b -> '1' | _ -> '0'
-
   let rate select =
     diagnosticReport
     |> List.map (frequencies >> select)
-    |> List.toArray
-    |> fun xs -> Convert.ToInt32 (String xs, 2)
-  in rate gamma * rate epsilon
+    |> number
+  in rate mostCommon * rate leastCommon
+
+let compute2 =
+  let diagnosticReport =
+    input
+    |> List.map List.ofSeq
+
+  let rec loop ix report select =
+    let mask =
+      report
+      |> List.transpose
+      |> List.map (frequencies >> select)
+      |> List.item ix
+
+    report
+    |> List.filter (fun bitmap -> (List.item ix bitmap) = mask)
+    |> function single :: [] -> number single
+              | remaining    -> loop (ix + 1) remaining select
+    
+  let oxygenGeneratorRating =
+    loop 0 diagnosticReport mostCommon
+  let co2ScrubberRating =
+    loop 0 diagnosticReport leastCommon
+  in co2ScrubberRating * oxygenGeneratorRating
--- Program.fs
+++ Program.fs
@@ -5,4 +5,5 @@ printfn "Day 1/ 2: %d" Day1.compute2
 
 printfn "Day 2/ 2: %d" Day2.compute
 
-printfn "Day 3/ 1 %A" Day3.compute
+printfn "Day 3/ 1 %A" Day3.compute1
+printfn "Day 3/ 2 %A" Day3.compute2
