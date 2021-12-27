182746f87759cfcbce6c70389a2e9773c714ad62
Patrik Andersson <patrik.andersson@bynk.se>
2021-12-27 13:59:33 +0100

    Day 7, second part.

--- Day-7.fs
+++ Day-7.fs
@@ -12,12 +12,19 @@ module Cluster =
     |> Array.map int 
     |> List.ofArray
 
-  let distanceSum (cluster : Cluster) (convergeance : Position) =
-    List.sumBy (fun pos -> abs (convergeance - pos)) cluster
+  let linear = id
 
-  let optimalCost (cluster : Cluster) =
+  let nonLinear distance =
+    let d = float distance
+    in (d / 2.) * (1. + d)
+       |> int
+
+  let fuelCost calculate (cluster : Cluster) (convergeance : Position) =
+    List.sumBy (fun pos -> calculate (abs (convergeance - pos))) cluster
+
+  let optimalCost pricing (cluster : Cluster) =
     [ List.min cluster .. List.max cluster ]
-    |> List.map (distanceSum cluster)
+    |> List.map (fuelCost pricing cluster)
     |> List.min
 
 module Test =
@@ -28,7 +35,13 @@ module Live =
   let input =
     IO.File.ReadAllText "input-7.txt"
 
-let compute =
+let compute strategy =
   Live.input
   |> Cluster.parse
-  |> Cluster.optimalCost
+  |> Cluster.optimalCost strategy
+
+let compute1 =
+  compute Cluster.linear
+
+let compute2 =
+  compute Cluster.nonLinear
--- Program.fs
+++ Program.fs
@@ -17,4 +17,5 @@ printfn "Day 5/ 2 %A" Day5.compute2
 printfn "Day 6/ 1 %A" Day6.compute1
 printfn "Day 6/ 2 %A" Day6.compute2
 
-printfn "Day 7/ 1 %A" Day7.compute
+printfn "Day 7/ 1 %A" Day7.compute1
+printfn "Day 7/ 2 %A" Day7.compute2
